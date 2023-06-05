ed04daf10f47dd27a45a667ffae61f9210788b40
jbarrera094 <jbarrera094@gmail.com>
2023-06-04 19:52:06 -0500

    update config

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
         dbConfig: {
             host: '127.0.0.1',
             port: 3306,
-            user: 'client',
-            password: '123456',
+            user: 'sammy',
+            password: 'col32spa98',
             database: 'bsenergy_lcheck'
         },
         secret: 'Space shield by BsEnergy and Developer Jey',
