2a5b3b96535cba01c9540437c77c065fa0b3810c
Jeisson H. Barrera Sanchez <jbarrera094@gmail.com>
2024-08-05 23:10:36 -0500

    test read variable

--- services/user.service.js
+++ services/user.service.js
@@ -1,78 +1,85 @@
-import { BehaviorSubject } from 'rxjs';
-import getConfig from 'next/config';
-import Router from 'next/router';
+import { BehaviorSubject } from "rxjs";
+import getConfig from "next/config";
+import Router from "next/router";
 
-import { fetchWrapper } from 'helpers';
-import { alertService } from './alert.service';
+import { fetchWrapper } from "helpers";
+import { alertService } from "./alert.service";
 
 const { publicRuntimeConfig } = getConfig();
-const baseUrl = `${publicRuntimeConfig.apiUrl}/users`;
-const userSubject = new BehaviorSubject(typeof window !== 'undefined' && JSON.parse(localStorage.getItem('user')));
+const baseUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/users`;
+const userSubject = new BehaviorSubject(
+  typeof window !== "undefined" && JSON.parse(localStorage.getItem("user"))
+);
 
 export const userService = {
-    user: userSubject.asObservable(),
-    get userValue() { return userSubject.value },
-    login,
-    logout,
-    register,
-    getAll,
-    getById,
-    update,
-    delete: _delete
+  user: userSubject.asObservable(),
+  get userValue() {
+    return userSubject.value;
+  },
+  login,
+  logout,
+  register,
+  getAll,
+  getById,
+  update,
+  delete: _delete,
 };
 
 async function login(email, password) {
-    const user = await fetchWrapper.post(`${baseUrl}/authenticate`, { email, password });
+  const user = await fetchWrapper.post(`${baseUrl}/authenticate`, {
+    email,
+    password,
+  });
 
-    // publish user to subscribers and store in local storage to stay logged in between page refreshes
-    userSubject.next(user);
-    localStorage.setItem('user', JSON.stringify(user));
+  // publish user to subscribers and store in local storage to stay logged in between page refreshes
+  userSubject.next(user);
+  localStorage.setItem("user", JSON.stringify(user));
 }
 
 function logout() {
-    alertService.clear();
-    // remove user from local storage, publish null to user subscribers and redirect to login page
-    localStorage.removeItem('user');
-    userSubject.next(null);
-    Router.push('/');
+  alertService.clear();
+  // remove user from local storage, publish null to user subscribers and redirect to login page
+  localStorage.removeItem("user");
+  userSubject.next(null);
+  Router.push("/");
 }
 
 async function register(user) {
-    await fetchWrapper.post(`${baseUrl}/register`, user);
+  await fetchWrapper.post(`${baseUrl}/register`, user);
 }
 
 async function registerLicense(user) {
-    await fetchWrapper.post(`${baseUrl}/register/license`, user);
+  await fetchWrapper.post(`${baseUrl}/register/license`, user);
 }
 
 async function getAll() {
-    return await fetchWrapper.get(baseUrl);
+  return await fetchWrapper.get(baseUrl);
 }
 
 async function getById(id) {
-    return await fetchWrapper.get(`${baseUrl}/${id}`);
+  return await fetchWrapper.get(`${baseUrl}/${id}`);
 }
 
 async function update(id, params) {
-    await fetchWrapper.put(`${baseUrl}/${id}`, params);
+  await fetchWrapper.put(`${baseUrl}/${id}`, params);
 
-    // update stored user if the logged in user updated their own record
-    if (id === userSubject.value.id) {
-        // update local storage
-        const user = { ...userSubject.value, ...params };
-        localStorage.setItem('user', JSON.stringify(user));
+  // update stored user if the logged in user updated their own record
+  if (id === userSubject.value.id) {
+    // update local storage
+    const user = { ...userSubject.value, ...params };
+    localStorage.setItem("user", JSON.stringify(user));
 
-        // publish updated user to subscribers
-        userSubject.next(user);
-    }
+    // publish updated user to subscribers
+    userSubject.next(user);
+  }
 }
 
 // prefixed with underscored because delete is a reserved word in javascript
 async function _delete(id) {
-    await fetchWrapper.delete(`${baseUrl}/${id}`);
+  await fetchWrapper.delete(`${baseUrl}/${id}`);
 
-    // auto logout if the logged in user deleted their own record
-    if (id === userSubject.value.id) {
-        logout();
-    }
+  // auto logout if the logged in user deleted their own record
+  if (id === userSubject.value.id) {
+    logout();
+  }
 }
