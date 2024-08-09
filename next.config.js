fd60d46e0d4fc3e955e652c55df5e487bbae9932
Jeisson H. Barrera Sanchez <jbarrera@energymaster.co>
2024-08-09 08:31:29 -0500

    chore: Update API and base URLs in next.config.js

--- next.config.js
+++ next.config.js
@@ -14,8 +14,8 @@ const nextConfig = {
     stripe_webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
   },
   publicRuntimeConfig: {
-    apiUrl: process.env.NEXT_PUBLIC_API_URL,
-    baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+    apiUrl: "http://lpscad.bsenergy.co/api",
+    baseUrl: "http://lpscad.bsenergy.co",
     pack1: process.env.NEXT_PUBLIC_PACK1,
     pack2_monthly: process.env.NEXT_PUBLIC_PACK2_MONTHLY,
     pack3_monthly: process.env.NEXT_PUBLIC_PACK3_MONTHLY,
