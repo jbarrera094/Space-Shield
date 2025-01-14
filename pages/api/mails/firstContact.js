c364dca30ef274a81e59324c0301476f502fcede
jbarrera094 <jbarrera094@gmail.com>
2025-01-13 23:17:59 -0500

    refactor: Mejorar la indentación y el formato del archivo firstContact.js

--- pages/api/mails/firstContact.js
+++ pages/api/mails/firstContact.js
@@ -1,10 +1,10 @@
-import { apiHandler, MailsRepo } from 'helpers/api';
+import { apiHandler, MailsRepo } from "helpers/api";
 
 export default apiHandler({
-    post: firstContact,
+  post: firstContact,
 });
 
 async function firstContact(req, res) {
-    const statusEmail = await MailsRepo.firstContact(req.body);
-    return res.status(200).json(statusEmail);
+  const statusEmail = await MailsRepo.firstContact(req.body);
+  return res.status(200).json(statusEmail);
 }
